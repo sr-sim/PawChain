4c16b94dc10d5bcd99040b8083dcc3107b290219
sr-sim <sooruan04@gmail.com>
2026-07-16 22:30:55 +0800

    push

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,18 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      chainId: 11155111,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+    },
+  },
 };
 
 export default config;
